87d468ca82fff84dfdce22b9ebf10450a1e85c99
Lothriiik <uanderson.silva@arapiraca.ufal.br>
2025-05-08 21:38:31 -0300

    feat: criação pasta backend

---
+++ backend/app/main.py
@@ -0,0 +1,18 @@
+# backend/app/main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
